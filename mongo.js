4e09deb544e13ae9298a1492058e4535a00c7b43
Aelderian <simikrefero@gmail.com>
2023-11-03 22:17:48 +0100

    Final vesion

--- mongo.js
+++ mongo.js
@@ -10,27 +10,29 @@ const password = process.argv[2]
 const url =
   `mongodb+srv://simikrefero:${password}@learning.qjafmlh.mongodb.net/noteApp?retryWrites=true&w=majority`
 
-mongoose.set('strictQuery',false)
-mongoose.connect(url)
 
-const noteSchema = new mongoose.Schema({
-  content: String,
-  important: Boolean,
-})
+console.log(`${url}`);
+// mongoose.set('strictQuery',false)
+// mongoose.connect(url)
 
-const Note = mongoose.model('Note', noteSchema)
-
-// const note = new Note({
-//   content: 'hcbvdf jghhgj',
-//   important: false,
+// const noteSchema = new mongoose.Schema({
+//   content: String,
+//   important: Boolean,
 // })
 
-Note.find({important: false}).then(result => {
-    result.forEach(note => {
-      console.log(note)
-    })
-    mongoose.connection.close()
-})
+// const Note = mongoose.model('Note', noteSchema)
+
+// // const note = new Note({
+// //   content: 'hcbvdf jghhgj',
+// //   important: false,
+// // })
+
+// Note.find({important: false}).then(result => {
+//     result.forEach(note => {
+//       console.log(note)
+//     })
+//     mongoose.connection.close()
+// })
 
 // note.save().then(result => {
 //   console.log('note saved!')
