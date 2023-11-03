d7fa431eda1c84036dd463314abd5ba01bd76db4
Aelderian <simikrefero@gmail.com>
2023-11-03 18:55:46 +0100

    Final vesion

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
+require('dotenv').config()
+
+const Note = require('./models/note')
+
 const express = require("express");
 const app = express();
 
 const cors = require('cors')
-
-
 
 let notes = [
   {
@@ -29,9 +31,9 @@ app.use(cors())
 app.use(express.json());
 app.use(express.static('dist'))
 
-app.get("/", (req, res) => {
-  res.send("<h1>Hello World!</h1>");
-});
+// app.get("/", (req, res) => {
+//   res.send("<h1>Hello World!</h1>");
+// });
 
 const generateId = () => {
   const maxId = notes.length > 0 ? Math.max(...notes.map((n) => n.id)) : 0;
@@ -59,9 +61,11 @@ app.post("/api/notes", (request, response) => {
   response.json(note);
 });
 
-app.get("/api/notes", (req, res) => {
-  res.json(notes);
-});
+app.get('/api/notes', (request, response) => {
+  Note.find({}).then(notes => {
+    response.json(notes)
+  })
+})
 
 app.delete("/api/notes/:id", (request, response) => {
   const id = Number(request.params.id);
@@ -81,7 +85,13 @@ app.get("/api/notes/:id", (request, response) => {
   }
 });
 
-const PORT = process.env.PORT || 3001
+const PORT = process.env.PORT 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
+
+
+//! MongoDB
+
+
+
